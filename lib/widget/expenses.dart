5f55a7bbf0a0d996682d79a2921f8a938693bbd2
José Carlos Bezerra <jcbsdev@MacBook-Air-de-Jose.local>
2023-09-19 23:07:55 -0300

    adding appBar and Modal overlay to add expenses

--- lib/widget/expenses.dart
+++ lib/widget/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/widget/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/widget/new_expense.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 
@@ -30,9 +31,25 @@ class _ExpensesState extends State<Expenses> {
         category: Category.work),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => const NewExpense(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Expense Tracker'),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
       body: Column(
         children: [
           const Text('The chart'),
